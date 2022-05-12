80ed7b542702b33142e489269f9f7349f1237eb8
root <root@L04307.infotrack.com.au>
2022-05-13 08:04:27 +1000

    work dev config

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 
-declare_id!("FwAQwZfCdjMmJjftbvcELYTbH6Xg1sadDTPe9d7MyABq");
+declare_id!("H649eEsXhsBggRq7jULojR1qf5igYMUHMjZYqafDeRMH");
+// declare_id!("FwAQwZfCdjMmJjftbvcELYTbH6Xg1sadDTPe9d7MyABq");
 
 #[program]
 pub mod solana_twitter {
